1b2e67d9b46aa7157bac4cfc8620d8e12ec543ef
Kamil Niemczyk <kamil.niemczyk@capgemini.com>
2015-07-23 16:06:32 +0200

    Added product() method to MathematicalOperations & written tests for it.

--- src/main/java/com/capgemini/MathematicalOperations/MathematicalOperations.java
+++ src/main/java/com/capgemini/MathematicalOperations/MathematicalOperations.java
@@ -1,10 +1,13 @@
 package com.capgemini.MathematicalOperations;
 
 /**
- * Hello world!
- *
+ * The MathematicalOperations implements four simple mathematical operations
  */
-public class MathematicalOperations 
-{
-
+public class MathematicalOperations {
+	/**
+	 * @return product of <b>arg0</b> and <b>arg1</b>
+	 */
+	public double product(double arg0, double arg1) {
+		return arg0*arg1;
+	}
 }
--- src/test/java/com/capgemini/MathematicalOperations/MathematicalOperationsTest.java
+++ src/test/java/com/capgemini/MathematicalOperations/MathematicalOperationsTest.java
@@ -1,13 +1,50 @@
 package com.capgemini.MathematicalOperations;
 
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class MathematicalOperationsTest 
-{
+public class MathematicalOperationsTest {
+	private MathematicalOperations mathOperObject = new MathematicalOperations();
+	private double arg0, arg1;
+
+	@Test
+	public void resultIs0IfAnyArgumentEquals0() {
+		// given
+		arg0 = 0;
+		arg1 = 1;
+		// then
+		assertTrue(mathOperObject.product(arg0, arg1) == 0.0);
+		assertTrue(mathOperObject.product(arg1, arg0) == 0.0);
+		assertTrue(mathOperObject.product(arg0, arg0) == 0.0);
+	}
+
+	@Test
+	public void resultIsArg0IfArg1Equals1() {
+		// given
+		arg0 = 10;
+		arg1 = 1;
+		// then
+		assertTrue(mathOperObject.product(arg0, arg1) == arg0);
+	}
+
+	@Test
+	public void resultIsNegativeIfOneArgumentIsPositiveAndOtherIsNegative() {
+		// given
+		arg0 = 11;
+		arg1 = -1;
+		// then
+		assertTrue(mathOperObject.product(arg0, arg1) < 0);
+		assertTrue(mathOperObject.product(arg1, arg0) < 0);
+	}
+
+	@Test
+	public void resultIsPositiveIfBothArg0AndArg1AreNegative() {
+		// given
+		arg0 = -10;
+		arg1 = -2;
+		// then
+		assertTrue(mathOperObject.product(arg0, arg1) > 0);
+		assertTrue(mathOperObject.product(arg1, arg0) > 0);
+	}
 
 }
