f7f54025fbd55eb491ab60cba3ecef675b4c223f
Kamil Niemczyk <kamil.niemczyk@capgemini.com>
2015-07-23 16:59:24 +0200

    Deleted unused import (@Before) in test

--- src/test/java/com/capgemini/MathematicalOperations/MathematicalOperationsTest.java
+++ src/test/java/com/capgemini/MathematicalOperations/MathematicalOperationsTest.java
@@ -3,7 +3,6 @@ package com.capgemini.MathematicalOperations;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class MathematicalOperationsTest {
