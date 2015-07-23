babf4c19fccbaeef2db8543f8c01ecf235f4af57
korczyn <logarytmzdwoch@gmail.com>
2015-07-23 10:02:09 +0200

    quadratic residue method and tests added

--- src/main/java/com/capgemini/MathematicalOperations/MathematicalOperations.java
+++ src/main/java/com/capgemini/MathematicalOperations/MathematicalOperations.java
@@ -1,10 +1,41 @@
 package com.capgemini.MathematicalOperations;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Hello world!
  *
  */
-public class MathematicalOperations 
-{
+public class MathematicalOperations {
+	/**
+	 * Calculates quadratic residues modulo number. Number should be 1 or
+	 * greater. Returns sorted list of unique quadratic residues
+	 * 
+	 * @author MKORCZYN
+	 * @param number
+	 *            - number of which quadratic residues we want to calculate
+	 * @return sorted list of unique quadratic residues
+	 * @throws IllegalArgumentException
+	 *             if number is 0 or lower
+	 */
+	public List<Integer> calculateQuadraticResidues(int number) {
+		List<Integer> list = new ArrayList<Integer>();
+		if (number < 1) {
+			throw new IllegalArgumentException("values must be positive");
+		}
 
+		Set<Integer> set = new HashSet<>();
+		for (int i = 0; i < number; i++) {
+			int qr = (i * i) % number;
+			set.add(qr);
+		}
+		list.addAll(set);
+		Collections.sort(list);
+		return list;
+
+	}
 }
--- src/test/java/com/capgemini/MathematicalOperations/MathematicalOperationsTest.java
+++ src/test/java/com/capgemini/MathematicalOperations/MathematicalOperationsTest.java
@@ -1,13 +1,42 @@
 package com.capgemini.MathematicalOperations;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class MathematicalOperationsTest 
-{
+public class MathematicalOperationsTest {
+	MathematicalOperations mo;
 
+	@Before
+	public void setUp() {
+		mo = new MathematicalOperations();
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldThrowIllegalArgumentExceptionWhenGivenNegativeNumber() {
+		mo.calculateQuadraticResidues(-5);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldThrowIllegalArgumentExceptionWhenGiven0() {
+		mo.calculateQuadraticResidues(0);
+	}
+
+	@Test
+	public void shouldReturnListOfSize1AndContainingOnlyValue0WhenGiven1() {
+		assertEquals(1, mo.calculateQuadraticResidues(1).size());
+		assertTrue(mo.calculateQuadraticResidues(1).contains(0));
+	}
+
+	@Test
+	public void shouldReturnListOfSize2AndContainingValues0And1WhenGiven2() {
+		assertEquals(2, mo.calculateQuadraticResidues(2).size());
+		assertTrue(mo.calculateQuadraticResidues(2).contains(0));
+		assertTrue(mo.calculateQuadraticResidues(2).contains(1));
+	}
 }
