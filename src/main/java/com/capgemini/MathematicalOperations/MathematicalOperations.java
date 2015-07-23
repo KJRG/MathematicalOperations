2d790d9bd34ed06b0d59f31a322c1f6efbd5332e
MATSUS <mateusz.sus@capgemini.com>
2015-07-23 08:50:32 +0200

    First commit. Created Maven project with junit dependencies

---
+++ src/main/java/com/capgemini/MathematicalOperations/MathematicalOperations.java
@@ -0,0 +1,10 @@
+package com.capgemini.MathematicalOperations;
+
+/**
+ * Hello world!
+ *
+ */
+public class MathematicalOperations 
+{
+
+}
---
+++ src/test/java/com/capgemini/MathematicalOperations/MathematicalOperationsTest.java
@@ -0,0 +1,13 @@
+package com.capgemini.MathematicalOperations;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MathematicalOperationsTest 
+{
+
+}
