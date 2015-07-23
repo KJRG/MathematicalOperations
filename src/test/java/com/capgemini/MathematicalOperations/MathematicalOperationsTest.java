068eb8f08d173e63603c91e0c64945f2c73a9588
MGORLAS <MGORLAS@CE04694.corp.capgemini.com>
2015-07-23 10:30:59 +0200

    Created new tests for method 'Sum'

--- src/test/java/com/capgemini/MathematicalOperations/MathematicalOperationsTest.java
+++ src/test/java/com/capgemini/MathematicalOperations/MathematicalOperationsTest.java
@@ -22,4 +22,16 @@ public class MathematicalOperationsTest
 	public void shouldReturn4For2_2(){
 		assertEquals(4, mo.sum(2,2));
 	}
+	@Test
+	public void shouldReturn0For0_0(){
+		assertEquals(0, mo.sum(0,0));
+	}
+	@Test
+	public void shouldReturn0Forminus2_plus2(){
+		assertEquals(0, mo.sum(-2,2));
+	}
+	@Test
+	public void shouldReturnminus4Forminus2_minus2(){
+		assertEquals(-4, mo.sum(-2,-2));
+	}
 }
