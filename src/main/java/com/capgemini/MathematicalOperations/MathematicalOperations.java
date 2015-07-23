897ae306237445d9e0919de115cd1f8af6b65c2d
Marzena <marzena.gorlas@gmail.com>
2015-07-23 16:36:56 +0200

    download changes

--- src/main/java/com/capgemini/MathematicalOperations/MathematicalOperations.java
+++ src/main/java/com/capgemini/MathematicalOperations/MathematicalOperations.java
@@ -1,9 +1,24 @@
 package com.capgemini.MathematicalOperations;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * The MathematicalOperations implements four simple mathematical operations
  */
 public class MathematicalOperations {
+	/**
+	 * 
+	 * @param number
+	 * @return absolute value of number
+	 */
+	public double abs(double number) {
+		return number < 0 ? number * -1 : number;
+	}
+	
 	/**
 	 * @param number1
 	 * @param number2
